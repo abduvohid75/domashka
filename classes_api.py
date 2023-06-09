6264264b60bfffee0f23c3c966a8a328a225d6f5
abduvohid75 <128813776+abduvohid75@users.noreply.github.com>
2023-06-09 13:37:12 +0500

    Update classes_api.py

--- classes_api.py
+++ classes_api.py
@@ -5,14 +5,11 @@ import json
 import os
 import time
 
-
-
 class SiteAPI(ABC):
 
     @abstractmethod
     def get_vacancies(self, search_query: str, keywords: str, top_vacancies: int):
         pass
-
 
 class HeadHunterAPI(SiteAPI):
 
@@ -54,32 +51,24 @@ class HeadHunterAPI(SiteAPI):
                     time.sleep(0.25)
             print(f"LOADING: 50 %")
 
-
 class SuperJobAPI(SiteAPI):
     def get_vacancies(self, search_query: str, keywords: str, top_vacancies: int):
         headers = {"X-Api-App-Id": 'v3.h.4470305.34df124d3610ceb6323baf8548815788c0c9031b.7a728b4d5c84b82bb14e5899a8991fdfe0fbff53'}
         superjob_api = 'https://api.superjob.ru/2.0/vacancies'
-
         page_number = 0
         pages_count = 1
 
         while page_number < pages_count:
-
             response = requests.get(superjob_api, headers= headers, params= f"keyword={search_query.replace(' ', '&')}"
                                                                           f"{keywords.replace(' ', '&')}"
                                                                           f"&page={page_number}"
                                                                           f"&count=100")
-
-
             response_json = response.json()
-
             file_write_jobs = 'sj_jobs.json'
-
 
             if len(response_json["objects"]) == 0:
                 time.sleep(0.25)
                 break
-
             else:
                 with open(file_write_jobs, 'a', encoding="utf-8") as add_file:
                     if os.stat(file_write_jobs).st_size == 0:
@@ -104,4 +93,3 @@ class SuperJobAPI(SiteAPI):
 
         print(f"LOADING: 100 %")
         time.sleep(0.25)
-
